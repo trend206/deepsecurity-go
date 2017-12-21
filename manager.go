42f3bc8280c926fa45772fbc885ae4182810e854
Jeff Thorne <jeff.thorne@gmail.com>
2017-12-21 10:07:13 -0500

    cleaning up error handling

--- manager.go
+++ manager.go
@@ -1,3 +1,5 @@
+// package deepsecurity provides a DSM struct to interface with and provides an abstraction
+// over Deep Security's REST and SOAP APIs
 package deepsecurity
 
 import (
@@ -10,6 +12,11 @@ import (
 	"errors"
 )
 
+
+var dsasHost string = "app.deepsecurity.trendmicro.com"
+var dsasPort string = "443"
+
+
 type DSM struct {
 	SessionID  string
 	Host       string
@@ -21,11 +28,10 @@ type DSM struct {
 	SoapURL    string
 }
 
-var dsasHost string = "app.deepsecurity.trendmicro.com"
-var dsasPort string = "443"
 
-
-func NewDSM(username string, password string, host string, port string, tenant string, verifySSL bool) DSM {
+// NewDSM is used to obtain a DSM struct.
+// return empty DSM struct if there was a problem with communication or auth.
+func NewDSM(username string, password string, host string, port string, tenant string, verifySSL bool) (DSM, error) {
 	dsm := DSM{Host: host, Port: port, Tenant: tenant}
 	if dsm.Host == "" {
 		dsm.Host = dsasHost
@@ -36,11 +42,17 @@ func NewDSM(username string, password string, host string, port string, tenant s
 	dsm.SoapURL = fmt.Sprintf("https://%s:%s/webservice/Manager", dsm.Host, dsm.Port)
 	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: !verifySSL}}
 	dsm.RestClient = http.Client{Transport: tr}
-	authenticate(username, password, &dsm, verifySSL)
-	return dsm
+	sessionID, err := authenticate(username, password, &dsm, verifySSL)
+
+	if err != nil{
+		return DSM{}, err
+	}else{
+		dsm.SessionID = sessionID
+		return dsm, nil
+	}
 }
 
-func authenticate(username string, password string, dsm *DSM, verifySSL bool) {
+func authenticate(username string, password string, dsm *DSM, verifySSL bool) (string, error) {
 
 	var str string
 	auth := gowsdlservice.BasicAuth{Login: username, Password: password}
@@ -51,21 +63,24 @@ func authenticate(username string, password string, dsm *DSM, verifySSL bool) {
 		str = fmt.Sprintf("{\"dsCredentials\":{\"tenantName\": \"%s\", \"password\": \"%s\", \"userName\": \"%s\"}}", dsm.Tenant, password, username)
 		authResponse, err := dsm.SoapClient.AuthenticateTenant(&tenantAuth)
 		if err != nil {
-			log.Fatal(err)
+			return "", err
+		}else{
+			return authResponse.AuthenticateTenantReturn, nil
 		}
-		dsm.SessionID = authResponse.AuthenticateTenantReturn
 	} else {
 		str = fmt.Sprintf("{\"dsCredentials\":{\"password\": \"%s\", \"userName\": \"%s\"}}", password, username)
 		url := fmt.Sprintf("%sauthentication/login", dsm.RestURL)
 		res, err := grequests.Post(url, &grequests.RequestOptions{HTTPClient: &dsm.RestClient, JSON: []byte(str), IsAjax: false})
 		if err != nil {
-			log.Fatalf("There was a problem %v", err)
+			return "", err
+		}else{
+			return res.String(), nil
 		}
-		dsm.SessionID = res.String()
 	}
 
 }
 
+// EndSession logs out of the session with the DSM
 func (dsm DSM) EndSession() {
 	url := fmt.Sprintf("%sauthentication/logout", dsm.RestURL)
 	_, err := grequests.Delete(url, &grequests.RequestOptions{HTTPClient: &dsm.RestClient, Params: map[string]string{"sID": dsm.SessionID}})
@@ -77,34 +92,8 @@ func (dsm DSM) EndSession() {
 }
 
 
-func (dsm DSM)HostClearWarningsErrors(hosts []int32) *gowsdlservice.HostClearWarningsErrorsResponse{
-	hce := gowsdlservice.HostClearWarningsErrors{HostIDs:hosts, SID:dsm.SessionID,}
-	response, _ := dsm.SoapClient.HostClearWarningsErrors(&hce)
-	return response
-}
-
-func (dsm DSM) HostGetStatus(host int32) *gowsdlservice.HostStatusTransport {
-	hgs := gowsdlservice.HostGetStatus{Id: host, SID: dsm.SessionID,}
-	response, err := dsm.SoapClient.HostGetStatus(&hgs)
-
-	if err != nil {
-		fmt.Println("in nill", err)
-
-	}
-
-	return response.HostGetStatusReturn
-}
 
 
-
-func (dsm DSM) HostRecommendationScan(hosts []int32){
-	hrs := gowsdlservice.HostRecommendationScan{HostIDs: hosts, SID: dsm.SessionID}
-	_, err := dsm.SoapClient.HostRecommendationScan(&hrs)
-	if err != nil{
-		log.Println("Error Initiating reccomentation scan:", err)
-	}
-
-}
 
 
 func (dsm DSM)HostRetrieveByName(hostName string) *gowsdlservice.HostTransport{
@@ -203,16 +192,7 @@ func (dsm DSM)DPIRuleRetrieve(ruleID int) (*gowsdlservice.DPIRuleTransport, erro
 }
 
 
-func (dsm DSM)HostGroupRetrieveAll() []*gowsdlservice.HostGroupRetrieveAllReturnTransport{
 
-	hgra := gowsdlservice.HostGroupRetrieveAll{SID: dsm.SessionID}
-	resp, err := dsm.SoapClient.HostGroupRetrieveAll(&hgra)
-	if err != nil{
-		log.Println("Error retreiving host group all:", err)
-	}
-
-	return resp.HostGroupRetrieveAllReturn
-}
 
 
 func (dsm DSM)HostMoveToHostGroup(hostIDs []int32, hostGroupID int32) *gowsdlservice.HostMoveToHostGroupResponse{
@@ -244,6 +224,8 @@ func (dsm DSM) HostGroupCreate(name string, external bool, externalID string, pa
 
 }
 
+// HostRetrieveAll retrieves all hosts from the DSM
+// returns empty slice if none found or error occurs
 func (dsm DSM) HostRetrieveAll() ([]*gowsdlservice.HostTransport, error){
 	hra := gowsdlservice.HostRetrieveAll{SID:dsm.SessionID}
 	resp, err := dsm.SoapClient.HostRetrieveAll(&hra)
@@ -252,11 +234,10 @@ func (dsm DSM) HostRetrieveAll() ([]*gowsdlservice.HostTransport, error){
 	}else{
 		return resp.HostRetrieveAllReturn, nil
 	}
-
 }
 
 // HostRetrieveByHostGroup retrieves all hosts in a dsm group by id.
-// It returns a slice of HostTransport obejcts if group ID is found or an empty list
+// returns empty slice if none found or error occurs
 func (dsm DSM) HostRetrieveByHostGroup(hostGroupId int) ([]*gowsdlservice.HostTransport, error){
 	hrgh := gowsdlservice.HostRetrieveByHostGroup{HostGroupID:int32(hostGroupId), SID:dsm.SessionID}
 	resp, err  := dsm.SoapClient.HostRetrieveByHostGroup(&hrgh)
@@ -265,6 +246,50 @@ func (dsm DSM) HostRetrieveByHostGroup(hostGroupId int) ([]*gowsdlservice.HostTr
 	}else{
 		return resp.HostRetrieveByHostGroupReturn, nil
 	}
-
 }
 
+// HostGroupRetrieveAll retreives all hostgroups
+// returns empty list if error or none found
+func (dsm DSM)HostGroupRetrieveAll() ([]*gowsdlservice.HostGroupRetrieveAllReturnTransport, error){
+	hgra := gowsdlservice.HostGroupRetrieveAll{SID: dsm.SessionID}
+	resp, err := dsm.SoapClient.HostGroupRetrieveAll(&hgra)
+	if err != nil{
+		return make([]*gowsdlservice.HostGroupRetrieveAllReturnTransport, 0), errors.New("Could not retreive host groups.")
+	}else{
+		return resp.HostGroupRetrieveAllReturn, nil
+	}
+}
+
+// HostClearWarningsErrors clears warning and errors on a single host of list of hosts
+// in all cases it returns an empty HostClearWarningsErrorsResponse object. note* I have yet to find error condition.
+func (dsm DSM)HostClearWarningsErrors(hosts []int32) *gowsdlservice.HostClearWarningsErrorsResponse{
+	hce := gowsdlservice.HostClearWarningsErrors{HostIDs:hosts, SID:dsm.SessionID,}
+	response, _ := dsm.SoapClient.HostClearWarningsErrors(&hce)
+	return response
+}
+
+// HostGetStatus retrieves a host status transport for a host by id
+// returns nil for HostStatusTransport if error found
+func (dsm DSM) HostGetStatus(host int32) (*gowsdlservice.HostStatusTransport, error) {
+	hgs := gowsdlservice.HostGetStatus{Id: host, SID: dsm.SessionID,}
+	response, err := dsm.SoapClient.HostGetStatus(&hgs)
+
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Unable to get host status for host: %d", host))
+	}else{
+		return response.HostGetStatusReturn, nil
+	}
+}
+
+// HostRecommendationScan initiates a host recommendation scan for an individual or list of hosts
+// returns nil if no error found
+func (dsm DSM) HostRecommendationScan(hosts []int32) error{
+	hrs := gowsdlservice.HostRecommendationScan{HostIDs: hosts, SID: dsm.SessionID}
+	_, err := dsm.SoapClient.HostRecommendationScan(&hrs)
+	if err != nil{
+		return errors.New(fmt.Sprintf("Error intiating host reccomentdation scan on hosts %d", hosts))
+	}else{
+		return nil
+	}
+
+}
